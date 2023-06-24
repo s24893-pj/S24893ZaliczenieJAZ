c4633b1030a82b813db1c403a8b1a2fffb537a31
s24893-pj <92208911+s24893-pj@users.noreply.github.com>
2023-06-24 15:06:34 +0200

    Add files via upload

---
+++ jaz-s24893-nbp/src/main/java/com/example/jazs24893nbp/JazS24893NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs24893nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS24893NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS24893NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-s24893-nbp/src/test/java/com/example/jazs24893nbp/JazS24893NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs24893nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS24893NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
